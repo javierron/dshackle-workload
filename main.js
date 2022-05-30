7ab6bccc80c3d03b9402010ffee5d59dd6f76a55
Javier Ron Arteaga <javierron90@gmail.com>
2022-05-30 17:00:19 +0200

    added request modes
    Signed-off-by: Javier Ron Arteaga <javierron90@gmail.com>

--- main.js
+++ main.js
@@ -3,20 +3,36 @@ const fs = require('fs')
 const http = require('http');
 
 require('toml-require').install();
-const config = require('./config.toml')
 const requests = require('./requests').workload_source
 
+const urls = {
+  geth: {
+    host: '172.17.0.1',
+    port: '8546',
+    path: '/'
+  },
+  besu: {
+    host: '172.17.0.1',
+    port: '8545',
+    path: '/'
+  },
+  dshackle: {
+    host: '172.17.0.2',
+    port: '8080',
+    path: '/eth'
+  }
+}
 
 const getRandomInt = max => {
   return Math.floor(Math.random() * max);
 }
 
-const sendRequest = (requestPayload, timed, file) => {
+const sendRequest = (target, requestPayload, file) => {
   return new Promise((resolve, _) => {
-
+    connection = urls[target]
     const options = {
-      hostname: config.connection.host,
-      port: config.connection.port,
+      hostname: connection.host,
+      port: connection.port,
       path: '/eth',
       method: 'POST',
       headers: {
@@ -36,8 +52,7 @@ const sendRequest = (requestPayload, timed, file) => {
       })
 
       res.on('end', () => {
-        // console.log(responseStr)
-        if (timed) {
+        if (file) {
           endTime = process.hrtime(startTime)
           file.write(`${requestPayload.params[0]},${endTime[1]}\n`)
         }
@@ -53,10 +68,9 @@ const sendRequest = (requestPayload, timed, file) => {
     })
 
     req.on('error', error => {
-      // console.log(error)
       resolve(`${requestPayload.method}: FAIL - ${error}`)
     })
-    
+
 
     req.write(payloadStr)
     req.end()
@@ -64,51 +78,109 @@ const sendRequest = (requestPayload, timed, file) => {
 
 }
 
-const testPass = () => {
+const testPass = target => {
   requests.forEach(item => {
-    sendRequest(item).then(console.log)
+    sendRequest(target, item).then(console.log)
   })
 }
 
-const randomPass = async times => {
-
+const randomPass = async (times, target) => {
   for (var i = 0; i < times; i++) {
     const data = requests[getRandomInt(requests.length)]
-    sendRequest(data).then(console.log)
-    await new Promise(resolve => setTimeout(resolve, 10));
+    await sendRequest(target, data).then(console.log)
   }
 }
 
-const readBlockExp = async times => {
-  const file = fs.createWriteStream('./data/latency.csv')
+const readBlockExp = async (times, target) => {
+  const file = fs.createWriteStream('./data/block-read-random.csv')
   for (var i = 0; i < times; i++) {
     const data = requests[0]
-    data.params = [`0x${randomInt(13000000).toString(16)}`, false]
-    sendRequest(data, true, file).then(console.log)
+    data.params[0] = `0x${randomInt(13000000).toString(16)}`
+    sendRequest(target, data, file).then(console.log)
     await new Promise(resolve => setTimeout(resolve, 20));
   }
   file.close()
 }
 
+const sequentialPass = async (target, limit) => {
+  const file = fs.createWriteStream('./data/block-read-sequential.csv')
+  for (var i = 0; i < limit; i++) {
+    const data = requests[0]
+    data.params[0] = `0x${i.toString(16)}`
+    await sendRequest(target, data, file).then(console.log)
+  }
+  file.close()
+}
+
+const cachePass = async (target, limit) => {
+  const file = fs.createWriteStream('./data/block-read-cache.csv')
+  const loops = limit / 100
+  for (var i = 0; i < loops; i++) {
+    const data = requests[0]
+    data.params[0] = `0x${i.toString(16)}`
+    for (var j = 0; j < 100; j++) {
+      await sendRequest(target, data, file).then(console.log)
+    }
+  }
+  file.close()
+}
+
+
 
 // main
 require('yargs/yargs')(process.argv.slice(2))
-  .usage('usage: $0 <command>')
-  .command('test', 'test the json payloads', argv => {
-    testPass()
-  })
+  .usage('usage: $0 <command> <target>')
   .command({
-    command: 'random <times>',
-    desc: 'execute random workload',
-    builder: yargs => yargs.default('times', 10),
+    command: 'test',
+    desc: 'test the json payloads',
+    builder: yargs => yargs
+      .choices('target', ['geth', 'besu', 'dshackle'])
+      .demandOption(['target']),
     handler: argv => {
-      randomPass(argv.times)
+      testPass(argv.target)
     }
   })
   .command({
-    command: 'readexp <times>',
+    command: 'random <times> <target>',
+    desc: 'execute random workload',
+    builder: yargs => yargs
+      .default('times', 100)
+      .choices('target', ['geth', 'besu', 'dshackle'])
+      .demandOption(['limit', 'target']),
+    handler: argv => {
+      randomPass(argv.target, argv.times)
+    }
+  })
+  .command({
+    command: 'sequential <limit> <target>',
+    desc: 'execute sequential workload',
+    builder: yargs => yargs
+      .default('limit', 100)
+      .choices('target', ['geth', 'besu', 'dshackle'])
+      .demandOption(['limit', 'target']),
+
+    handler: argv => {
+      sequentialPass(argv.target, argv.times)
+    }
+  })
+  .command({
+    command: 'cache <limit> <target>',
+    desc: 'execute cache-friendly workload',
+    builder: yargs => yargs
+      .default('times', 100)
+      .choices('target', ['geth', 'besu', 'dshackle'])
+      .demandOption(['limit', 'target']),
+    handler: argv => {
+      cachePass(argv.target, argv.times)
+    }
+  })
+  .command({
+    command: 'readblocks <times>',
     desc: 'read block experiment',
-    builder: yargs => yargs.default('times', 10),
+    builder: yargs => yargs
+      .default('times', 10)
+      .choices('target', ['geth', 'besu', 'dshackle'])
+      .demandOption(['times', 'target']),
     handler: argv => {
       readBlockExp(argv.times)
     }
